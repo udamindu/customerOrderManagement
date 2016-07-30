a2751a817bb355b531cc205a35595e195dcf8d78
UDamindu <daminduuthpala@gmail.com>
2016-07-30 11:59:29 +0530

    EditCustomer servlet done

---
+++ CustomerOrderManagementApplication/CustomerOrderManagementApplication-war/src/java/web/EditCustomer.java
@@ -0,0 +1,122 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package web;
+
+import ejb.Customer;
+import ejb.CustomerFacade;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.ejb.EJB;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Arunalu Hasakelum
+ */
+@WebServlet(name = "EditCustomer", urlPatterns = {"/EditCustomer"})
+public class EditCustomer extends HttpServlet {
+    @EJB
+    private CustomerFacade customerFacade;
+
+    /**
+     * Processes requests for both HTTP
+     * <code>GET</code> and
+     * <code>POST</code> methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+            /*
+             * TODO output your page here. You may use following sample code.
+             */
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet EditCustomer</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet EditCustomer at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        } finally {            
+            out.close();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP
+     * <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP
+     * <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //processRequest(request, response);
+        String name = request.getParameter("name");
+        String address = request.getParameter("address");
+        String contact = request.getParameter("contact");
+        String message;
+        Customer customer;
+        
+        if((name != null) && (address != null) && (contact != null)){
+            customer = new Customer();
+            customer.setName(name);
+            customer.setAddress(address);
+            customer.setContactNumber(contact);
+            customerFacade.edit(customer);
+            message = "Customer edited successfully!";
+            request.setAttribute("message", message);
+	    RequestDispatcher rd = getServletContext().getRequestDispatcher("/editCustomer.jsp");
+            rd.forward(request, response);
+        }
+        else{
+            message = "Failed to edit, try filling all fields!";
+            request.setAttribute("message", message);
+	    RequestDispatcher rd = getServletContext().getRequestDispatcher("/editCustomer.jsp");
+            rd.forward(request, response);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
